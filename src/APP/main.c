0c26c35a1447ab76365586d62b9c78286d90a46d
MayadaMetwally <mayadaelbarhemy2019@gmail.com>
2024-03-19 14:18:57 +0200

    NewRepo

---
+++ src/APP/main.c
@@ -0,0 +1,22 @@
+#include "STD_TYPES.h"
+#include "HLED/LED.h"
+#include "HSWITCH/SWITCH.h"
+#include "SERVICE/SCHED/SCHED.h"
+
+
+
+
+int main()
+{
+
+HLED_Init();
+HSWITCH_Init();
+
+Sched_Init();
+StartSched();
+
+return 0;
+
+}
+
+// ----------------------------------------------------------------------------
