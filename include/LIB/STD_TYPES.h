fa5c3cff4ec6673c310ffde8a76d66db7e369cfc
MayadaMetwally <mayadaelbarhemy2019@gmail.com>
2024-05-09 03:03:28 +0300

    add FreeRTOS

--- include/LIB/STD_TYPES.h
+++ include/LIB/STD_TYPES.h
@@ -4,7 +4,7 @@
 
 typedef unsigned char u8;
 typedef unsigned short int u16;
-typedef unsigned long  int u32;
+typedef unsigned long int u32;
 
 typedef signed char s8;
 typedef signed short int s16;
@@ -14,17 +14,6 @@ typedef float  f32;
 typedef double f64;
 
 typedef unsigned long long  u64;
-
-typedef unsigned char       uint8_t;
-typedef unsigned short      uint16_t;
-typedef unsigned int        uint32_t;
-typedef unsigned long long  uint64_t;
-typedef signed char         sint8_t;
-typedef signed short int    sint16_t;
-typedef signed int          sint32_t;
-typedef signed long long    sint64_t;
-typedef float               flt32_t;
-typedef double              flt64_t;
 
 
 typedef enum{
